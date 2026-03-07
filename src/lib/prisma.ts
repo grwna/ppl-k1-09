1fea1977dff500ae66cd4f5035c69dc2ec1b2046
grwna <13523035@std.stei.itb.ac.id>
2026-03-07 13:00:05 +0700

    feat(init): initialized templates and more tech stacks

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from '../generated/prisma'
+import { PrismaPg } from '@prisma/adapter-pg'
+import pg from 'pg'
+
+const connectionString = `${process.env.DATABASE_URL}`
+
+const pool = new pg.Pool({ connectionString })
+const adapter = new PrismaPg(pool)
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    datasource: {
-      url: process.env.DATABASE_URL,
-    },
-  })
+  new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatCurrency(amount: number) {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(amount)
+}
+
+// implement other utilities needed here...
