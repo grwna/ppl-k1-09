114db2ef4dee496415c8e56ca20294ad4204fb03
grwna <13523035@std.stei.itb.ac.id>
2026-03-10 17:02:50 +0700

    fix: resolve CI/CD failures

--- vercel-deployment.sh
+++ vercel-deployment.sh
@@ -8,11 +8,5 @@ if [[ "$VERCEL_GIT_COMMIT_REF" != "main" && "$VERCEL_GIT_COMMIT_REF" != "dev" ]]
   exit 0
 fi
 
-# Note: VERCEL_GIT_PREVIOUS_SHA is a system variable for the last successful build
-if git diff --quiet $VERCEL_GIT_PREVIOUS_SHA HEAD .; then
-  echo "🛑 - No code changes detected in this directory. Skipping."
-  exit 0
-fi
-
 echo "✅ - Branch is tracked and code changed. Proceeding with build..."
 exit 1
