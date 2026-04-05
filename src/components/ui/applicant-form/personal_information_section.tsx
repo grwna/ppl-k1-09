db9f9cbea836e4c09840d3173ee81bfc58bacba8
BrianHadianSTEI23 <bri.hadian@gmail.com>
2026-04-05 10:56:10 +0700

    feat: add personal information section

--- src/components/ui/applicant-form/personal_information_section.tsx
+++ src/components/ui/applicant-form/personal_information_section.tsx
@@ -1,9 +1,99 @@
 
+import { useApplicationProgressStore } from "@/hooks/applicationProgressStore";
 
 export default function ApplicantForm_PersonalInformationSection() {
 
+    const applicationProgress = useApplicationProgressStore((state) => (state.application_progress))
+    const full_name = useApplicationProgressStore((state) => (state.full_name))
+    const university_name = useApplicationProgressStore((state) => (state.university_name))
+    const student_id_number = useApplicationProgressStore((state) => (state.student_id_number))
+
     return (
         <div>
+
+            {/* title */}
+            <div className="flex w-full h-fit">
+                Personal Information
+            </div>
+
+            {/* caption */}
+            <div className="flex w-full h-fit">
+                Please provide your basic information as a student
+            </div>
+
+            {/* full name section */}
+            <div className="flex w-full h-fit">
+                
+                {/* title */}
+                <div>
+                    Full name
+                </div>
+
+                {/* input */}
+                <div>
+                    <input
+                        value={String(full_name)}
+                        onChange={(e) => useApplicationProgressStore((state) => {state.setFullName(e.target.value)})}
+                        onKeyDown={(e) => e.key === "Enter"}
+                        className={ `flex gap-2 border border-black/20 bg-white p-4 w-full h-[40%] rounded-2xl shadow-2xl`}
+                        placeholder="Masukkan nama anda..."
+                    />
+                </div>
+            </div>
+
+            {/* university section */}
+            <div className="flex w-full h-fit">
+                
+                {/* title */}
+                <div>
+                    University
+                </div>
+
+                {/* input */}
+                <div>
+                    <input
+                        value={String(university_name)}
+                        onChange={(e) => useApplicationProgressStore((state) => {state.setUniversityName(e.target.value)})}
+                        onKeyDown={(e) => e.key === "Enter"}
+                        className={ `flex gap-2 border border-black/20 bg-white p-4 w-full h-[40%] rounded-2xl shadow-2xl`}
+                        placeholder="Masukkan universitas anda..."
+                    />
+                </div>
+            </div>
+
+            {/* student id number section */}
+            <div className="flex w-full h-fit">
+                
+                {/* title */}
+                <div>
+                    Full name
+                </div>
+
+                {/* input */}
+                <div>
+                    <input
+                        value={String(student_id_number)}
+                        onChange={(e) => useApplicationProgressStore((state) => {state.setStudentIdNumber(e.target.value)})}
+                        onKeyDown={(e) => e.key === "Enter"}
+                        className={ `flex gap-2 border border-black/20 bg-white p-4 w-full h-[40%] rounded-2xl shadow-2xl`}
+                        placeholder="Masukkan nomor induk mahasiswa anda..."
+                    />
+                </div>
+            </div>
+
+            {/* cta button */}
+            <div className="flex w-full h-fit">
+
+                {/* back button */}
+                <div>
+                    Back
+                </div>
+
+                {/* continue button */}
+                <div>
+                    Continue
+                </div>
+            </div>
 
         </div>
     );
--- src/hooks/applicationProgressStore.ts
+++ src/hooks/applicationProgressStore.ts
@@ -6,15 +6,38 @@ import { ApplicationProgress } from "@/types/application_progress"
 type ApplicationProgressStore = {
     // these are the variables that the store store
     application_progress : ApplicationProgress | null
+    full_name : string | null
+    university_name : string | null
+    student_id_number : string | null
 
     // these are functions to call and define the create() section of zustand
     setStep : (step : number) => void
+    setFullName : (full_name : string) => void
+    setUniversityName : (university_name : string) => void
+    setStudentIdNumber : (student_id_number : string) => void
 }
 
 export const useApplicationProgressStore = create<ApplicationProgressStore>((set) => ({
     application_progress : null,
+    full_name : null,
+    university_name : null,
+    student_id_number : null,
     
     setStep(step) {
         set({ application_progress : {step} })
-    }
+    },
+    
+    setFullName(full_name) {
+        set({ full_name : full_name })
+    },
+    
+    setUniversityName(university_name) {
+        set({ university_name : university_name })
+    },
+    
+    setStudentIdNumber(student_id_number) {
+        set({ student_id_number : student_id_number })
+    },
+
+
 }))
