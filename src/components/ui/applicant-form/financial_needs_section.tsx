883e16079f87cf152d70576c6f45157a7e74cf45
BrianHadianSTEI23 <bri.hadian@gmail.com>
2026-04-05 11:06:54 +0700

    feat: add financial needs section + modify application progress store

--- src/components/ui/applicant-form/financial_needs_section.tsx
+++ src/components/ui/applicant-form/financial_needs_section.tsx
@@ -1,9 +1,99 @@
 
+import { useApplicationProgressStore } from "@/hooks/applicationProgressStore"
 
 export default function ApplicantForm_FinancialNeedsSection() {
 
+    const applicationProgress = useApplicationProgressStore((state) => (state.application_progress))
+    const loan_title = useApplicationProgressStore((state) => (state.loan_title))
+    const requested_amount = useApplicationProgressStore((state) => (state.requested_amount))
+    const loan_purpose = useApplicationProgressStore((state) => (state.loan_purpose))
+
     return (
         <div>
+
+            {/* title */}
+            <div className="flex w-full h-fit">
+                Financial Needs
+            </div>
+
+            {/* caption */}
+            <div className="flex w-full h-fit">
+                Tell us about your loan requirements
+            </div>
+
+            {/* full name section */}
+            <div className="flex w-full h-fit">
+                
+                {/* title */}
+                <div>
+                    Loan Title
+                </div>
+
+                {/* input */}
+                <div>
+                    <input
+                        value={String(loan_title)}
+                        onChange={(e) => useApplicationProgressStore((state) => {state.setLoanTitle(e.target.value)})}
+                        onKeyDown={(e) => e.key === "Enter"}
+                        className={ `flex gap-2 border border-black/20 bg-white p-4 w-full h-[40%] rounded-2xl shadow-2xl`}
+                        placeholder="Masukkan nama pinjaman anda..."
+                    />
+                </div>
+            </div>
+
+            {/* requested amount section */}
+            <div className="flex w-full h-fit">
+                
+                {/* title */}
+                <div>
+                    Jumlah yang diajukan (Rp)
+                </div>
+
+                {/* input */}
+                <div>
+                    <input
+                        value={String(requested_amount)}
+                        onChange={(e) => useApplicationProgressStore((state) => {state.setRequestedAmount(Number(e.target.value))})}
+                        onKeyDown={(e) => e.key === "Enter"}
+                        className={ `flex gap-2 border border-black/20 bg-white p-4 w-full h-[40%] rounded-2xl shadow-2xl`}
+                        placeholder="Masukkan universitas anda..."
+                    />
+                </div>
+            </div>
+
+            {/* loan purpose section */}
+            <div className="flex w-full h-fit">
+                
+                {/* title */}
+                <div>
+                    Loan Purpose
+                </div>
+
+                {/* input */}
+                <div>
+                    <input
+                        value={String(loan_purpose)}
+                        onChange={(e) => useApplicationProgressStore((state) => {state.setLoanPurpose(e.target.value)})}
+                        onKeyDown={(e) => e.key === "Enter"}
+                        className={ `flex gap-2 border border-black/20 bg-white p-4 w-full h-[40%] rounded-2xl shadow-2xl`}
+                        placeholder="Jelaskan tujuan pengajuan pinjaman anda..."
+                    />
+                </div>
+            </div>
+
+            {/* cta button */}
+            <div className="flex w-full h-fit">
+
+                {/* back button */}
+                <div>
+                    Back
+                </div>
+
+                {/* continue button */}
+                <div>
+                    Continue
+                </div>
+            </div>
 
         </div>
     );
--- src/hooks/applicationProgressStore.ts
+++ src/hooks/applicationProgressStore.ts
@@ -6,27 +6,49 @@ import { ApplicationProgress } from "@/types/application_progress"
 type ApplicationProgressStore = {
     // these are the variables that the store store
     application_progress : ApplicationProgress | null
+
+    // personal information
     full_name : string | null
     university_name : string | null
     student_id_number : string | null
+    
+    // financial needs
+    loan_title : string | null
+    requested_amount : number | null
+    loan_purpose : string | null
 
     // these are functions to call and define the create() section of zustand
     setStep : (step : number) => void
+
+    // personal information
     setFullName : (full_name : string) => void
     setUniversityName : (university_name : string) => void
     setStudentIdNumber : (student_id_number : string) => void
+
+    // financial needss
+    setLoanTitle : (loan_title : string) => void
+    setRequestedAmount : (requested_amount : number) => void
+    setLoanPurpose : (loan_purpose : string) => void
 }
 
 export const useApplicationProgressStore = create<ApplicationProgressStore>((set) => ({
     application_progress : null,
+    
+    // personal information
     full_name : null,
     university_name : null,
     student_id_number : null,
+    
+    // financial needs
+    loan_title : null,
+    requested_amount : null,
+    loan_purpose : null,
     
     setStep(step) {
         set({ application_progress : {step} })
     },
     
+    // personal information
     setFullName(full_name) {
         set({ full_name : full_name })
     },
@@ -37,6 +59,19 @@ export const useApplicationProgressStore = create<ApplicationProgressStore>((set
     
     setStudentIdNumber(student_id_number) {
         set({ student_id_number : student_id_number })
+    },
+    
+    // financial needs
+    setLoanTitle(loan_title) {
+        set({ loan_title : loan_title })
+    },
+    
+    setRequestedAmount(requested_amount) {
+        set({ requested_amount : requested_amount })
+    },
+    
+    setLoanPurpose(loan_purpose) {
+        set({ loan_purpose : loan_purpose })
     },
 
 
